128148b368520a90f1acdc2b82aeb1055b11f95b
Micha Birklbauer <micha.birklbauer@gmail.com>
2023-10-29 00:36:59 +0200

    fix strings

--- clean.py
+++ clean.py
@@ -4,5 +4,5 @@ import re
 
 def clean(text: str, keep_sentences: bool = True) -> str:
     if keep_sentences:
-        return re.sub("[^a-zA-Z\.\?\!\s]+", "", text)
-    return re.sub("[^a-zA-Z\s]+", "", text)
+        return re.sub(r"[^a-zA-Z\.\?\!\s]+", "", text)
+    return re.sub(r"[^a-zA-Z\s]+", "", text)
